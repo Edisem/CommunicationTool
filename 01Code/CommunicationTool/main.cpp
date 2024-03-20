c471a6ed412da53e26cc6f396cef5b8420b8bb0b
wangrui <2267009516@qq.com>
2024-03-20 21:35:33 +0800

    更新main.cpp

--- 01Code/CommunicationTool/main.cpp
+++ 01Code/CommunicationTool/main.cpp
@@ -1,11 +1,13 @@
 #include "widget.h"
 
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Widget w;
     w.show();
+    qDebug()<<"Hello World!";
     return a.exec();
 }
